173c7a6dde3e20c2fec87e3c9c1a3ffe7925bd24
cristianscode <cristianscode@gmail.com>
2021-01-01 23:11:21 -0800

    Component Navigation using React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import BarChart from "./_components/chart-types/bar-chart"
+import PieChart from "./_components/chart-types/pie-chart"
+import LineChart from "./_components/chart-types/line-chart"
+import DoughnutChart from "./_components/chart-types/doughnut-chart"
+import ChartSelection from "./_components/chart-selection"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
-        <h1>Main App</h1>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={ChartSelection} />
+                <Route path="/bar" component={BarChart} />
+                <Route path="/pie" component={PieChart} />
+                <Route path="/line" component={LineChart} />
+                <Route path="/doughnut" component={DoughnutChart} />
+            </Switch>
+        </Router>
     )
 }
-
 export default App;
---
+++ src/_components/chart-selection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from "react-router-dom";
+function ChartSelection() {
+    return (
+        <div>
+            <Link to="/bar"><h1>Bar</h1></Link>
+            <Link to="/pie"><h1>Pie</h1></Link>
+            <Link to="/line"><h1>Line</h1></Link>
+            <Link to="/doughnut"><h1>Doughnut</h1></Link>
+        </div>
+    )
+}
+
+export default ChartSelection
